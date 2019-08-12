9adb2eb162f81e776749310a4a15aec31a2173a9
Madura Eeswara <madura.madushan@outlook.com>
2019-08-12 18:37:11 +0530

    redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
 import 'jquery/dist/jquery'
 import 'popper.js/dist/popper'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers'
+
+const initialState = {}
+const middleware = [thunk]
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
